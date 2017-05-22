66976dae423ccd7a415f57ea5a943cfe280264c1
fengyoung <fengyoung82@sina.com>
2017-05-22 19:34:52 +0800

    fix bugs

--- video_embedding/image_embedding/cat_dog.py
+++ video_embedding/image_embedding/cat_dog.py
@@ -3,68 +3,75 @@ sys.path.append("../")
 
 import os
 import numpy as np
-import image_retrival
+import image_retrieval
+
+
+def cat_dog_demo(feats_test_file, feats_train_file):
+	test_feats = image_retrieval.read_features(feats_test_file)
+	train_feats = image_retrieval.read_features(feats_train_file)
+	
+	v_cat2 = np.array(test_feats[0][2])
+	v_cat1dog1 = np.array(test_feats[1][2])
+	v_cat1 = np.array(test_feats[2][2])
+	v_dog1 = np.array(test_feats[3][2])
+	v_dog2 = np.array(test_feats[4][2])
+	v1 = v_cat1dog1 - v_dog1
+	v2 = v_cat1dog1 - v_dog2
+
+	v_query = v_cat1dog1
+#	v_query = v1
+#	v_query = v2
+#	v_query = v_cat1
+#	v_query = v_cat2
+	
+	sim_vals = image_retrieval_by_feat(v_query, test_feats, sim_type): 
+	topn = np.min([6, len(sim_vals)])
+	for t in range(topn): 
+		print("top %d | sim: %.6g | id: %s, file: %s" % (t+1, sim_vals[t][2], sim_vals[t][0], sim_vals[t][1]))
+	tt = range(topn)
+	tt.reverse()
+	for t in tt:
+		os.system('open %s' % sim_vals[t][1])
+
+
+def car_salon_girl_demo(feats_test_file, feats_train_file):
+	test_feats = image_retrieval.read_features(feats_test_file)
+	train_feats = image_retrieval.read_features(feats_train_file)
+	
+	v_car = np.array(test_feats[0][2])
+	v_girl = np.array(test_feats[1][2])
+	v_car_and_girl = np.array(test_feats[2][2])
+	v1 = v_car + v_girl
+	v2 = v_car_and_girl - v_girl
+
+	v_query = v_car_and_girl
+#	v_query = v1
+#	v_query = v2
+#	v_query = v_girl
+#	v_query = v_car
+	
+	sim_vals = image_retrieval_by_feat(v_query, test_feats, sim_type): 
+	topn = np.min([6, len(sim_vals)])
+	for t in range(topn): 
+		print("top %d | sim: %.6g | id: %s, file: %s" % (t+1, sim_vals[t][2], sim_vals[t][0], sim_vals[t][1]))
+	tt = range(topn)
+	tt.reverse()
+	for t in tt:
+		os.system('open %s' % sim_vals[t][1])
 
 
 
-def cat_dog_demo(feats_train):
-	image_feats = image_retrieval.read_features("./model/02.feats")
+if __name__ == "__main__":
+	if len(sys.argv) != 4:
+		print("usage: cat_dog.py --cat_dog <test_feats_file> <train_feats_file>")
+		print("                  --car_salon_girl <test_feats_file> <train_feats_file>")
+		exit(-1)
+
+	if sys.argv[1] == "--cat_dog":
+		cat_dog_demo(sys.argv[2], sys.argv[3])
+	elif sys.argv[1] == "--car_salon_girl":
+		car_salon_girl_demo(sys.argv[2], sys.argv[3])
 
 
-
-
-def read_features(image_feat_file):
-	image_feats = []	
-	fp = open(image_feat_file, 'r')
-	line = fp.readline()
-	while line:
-		if len(line.rstrip()) > 0:
-			ss = line.rstrip().split('\t')
-			feats = map(lambda x: float(x), ss[2].split(','))				
-			image_feats.append([ss[0], ss[1], feats])
-		line = fp.readline()
-	fp.close()
-	return image_feats
-
-
-image_feats = read_features("./model/02.feats")
-train_feats = read_features("./model/01_train_20170419.feats")
-
-v_cat2 = np.array(image_feats[0][2])
-v_cat1dog1 = np.array(image_feats[1][2])
-v_cat1 = np.array(image_feats[2][2])
-v_dog1 = np.array(image_feats[3][2])
-v_dog2 = np.array(image_feats[4][2])
-v1 = v_cat1dog1 - v_dog1
-v2 = v_cat1dog1 - v_dog2
-
-
-print("sim(v1, v_cat1) = %.6g" % cosine_sim(v1, v_cat1))
-print("sim(v1, v_cat2) = %.6g" % cosine_sim(v1, v_cat2))
-print("sim(v2, v_cat1) = %.6g" % cosine_sim(v2, v_cat1))
-print("sim(v2, v_cat2) = %.6g" % cosine_sim(v2, v_cat2))
-
-
-v_query = v_cat1dog1
-#v_query = v1
-#v_query = v2
-#v_query = v_cat1
-#v_query = v_cat2
-
-sim_vals = []	
-for tfeat in train_feats:
-	sim = cosine_sim(v_query, np.array(tfeat[2]))
-	sim_vals.append([tfeat[0], tfeat[1], sim])
-sim_vals = sorted(sim_vals, cmp = lambda x, y: cmp(x[2], y[2]), reverse = True)
-topn = np.min([10, len(sim_vals)])
-for t in range(topn): 
-	print("top %d | sim: %.6g | id: %s, file: %s" % (t+1, sim_vals[t][2], sim_vals[t][0], sim_vals[t][1]))
-tt = range(topn)
-tt.reverse()
-for t in tt:
-	os.system('open %s' % sim_vals[t][1])
-
-
-
-
+	exit(0)
 
--- video_embedding/image_embedding/image_retrieval.py
+++ video_embedding/image_embedding/image_retrieval.py
@@ -74,43 +74,26 @@ def image_sim_match(feat1, feat2, sim_type = 'cosine'):
 	else:
 		return False
 
-		
-def run_image_sim_match(graph_file, image, image_feats): 
+	
+def image_retrieval_by_feat(src_image_feat_vec, image_feats, sim_type): 
+	sim_vals = []	
+	for imf in image_feats:
+		sim = image_sim_match(src_image_feat_vec, imf[2], sim_type)
+		sim_vals.append([imf[0], imf[1], sim])
+	sim_vals = sorted(sim_vals, cmp = lambda x, y: cmp(x[2], y[2]), reverse = True)
+	return sim_vals	
+	
+	
+def run_image_sim_match(graph_file, image, image_feats, sim_type): 
 	image2vec.create_graph(graph_file)
 	with tf.Session() as sess:
 		if gfile.Exists(image) == True:
 			image_data = gfile.FastGFile(image, 'rb').read()
 			feature_tensor = sess.graph.get_tensor_by_name('pool_3:0')
-			feats = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
-			feats = np.squeeze(feats)
-			for sim_type in ['cosine', 'euclidean', 'manhattan', 'chebyshev']:
-				sim_vals = []	
-				for im in image_feats:
-					sim = image_sim_match(feats, im[2], sim_type)
-					sim_vals.append([im[0], im[1], sim])
-				sim_vals = sorted(sim_vals, cmp = lambda x, y: cmp(x[2], y[2]), reverse = True)
-				print("SIM_TYPE: %s" % sim_type)
-				print("IMAGE: %s" % image)
-				for t in range(np.min([5, len(sim_vals)])):
-					print("top %d | sim: %.6g | id: %s, file: %s" % (t+1, sim_vals[t][2], sim_vals[t][0], sim_vals[t][1]))
-		else:
-			print("file does exist %s!" % image)
-			tf.logging.fatal('File does not exist %s', image)
-
-
-def run_image_sim_match2(graph_file, image, image_feats, sim_type): 
-	image2vec.create_graph(graph_file)
-	with tf.Session() as sess:
-		if gfile.Exists(image) == True:
-			image_data = gfile.FastGFile(image, 'rb').read()
-			feature_tensor = sess.graph.get_tensor_by_name('pool_3:0')
-			feats = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
-			feats = np.squeeze(feats)
-			sim_vals = []	
-			for im in image_feats:
-				sim = image_sim_match(feats, im[2], sim_type)
-				sim_vals.append([im[0], im[1], sim])
-			sim_vals = sorted(sim_vals, cmp = lambda x, y: cmp(x[2], y[2]), reverse = True)
+			feat_vec = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
+			feat_vec = np.squeeze(feat_vec)
+			
+			sim_vals = image_retrieval_by_feat(feat_vec, image_feats, sim_type)
 			print("SIM_TYPE: %s" % sim_type)
 			print("IMAGE: %s" % image)
 			topn = np.min([5, len(sim_vals)])
@@ -127,17 +110,13 @@ def run_image_sim_match2(graph_file, image, image_feats, sim_type):
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 4 and len(sys.argv) != 5:
-		print('usage: %s <model> <image_features> <image> <|sim_type>' % sys.argv[0])
+	if len(sys.argv) != 5: 
+		print('usage: %s <model> <image_features> <image> <sim_type>' % sys.argv[0])
 		print('sim_type: cosine, euclidean, manhattan or chebyshev')
 		exit(-1)
 
 	image_feats = read_features(sys.argv[2])
-
-	if len(sys.argv) == 4:
-		run_image_sim_match(sys.argv[1], sys.argv[3], image_feats)
-	elif len(sys.argv) == 5:
-		run_image_sim_match2(sys.argv[1], sys.argv[3], image_feats, sys.argv[4])
+	run_image_sim_match(sys.argv[1], sys.argv[3], image_feats, sys.argv[4])
 
 	exit(0)
 
